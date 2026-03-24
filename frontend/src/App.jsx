ba9a900c6814337b0c8a0524f69bdf3900e72ae8
MnaboDe <marinkbo@gmail.com>
2026-03-24 11:00:44 +0100

    Add lobby screen component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import Lobby from "./screens/Lobby";
+
 function App() {
   return (
     <main>
       <h1>Turn-Based Game</h1>
+      <Lobby />
       <button>Find Match</button>
     </main>
   );
---
+++ frontend/src/screens/Lobby.jsx
@@ -0,0 +1,10 @@
+function Lobby() {
+    return (
+        <div>
+            <h2>Lobby</h2>
+            <p>Press button to find a match</p>
+        </div>
+    );
+}
+
+export default Lobby;
