7b9a9d630668f5f0cfe03ebe5551fdefceb4ad5d
MnaboDe <marinkbo@gmail.com>
2026-03-24 11:19:32 +0100

    Style game screen

--- frontend/src/screens/Game.jsx
+++ frontend/src/screens/Game.jsx
@@ -1,6 +1,8 @@
+import "./Game.css";
+
 function Game() {
   return (
-    <div>
+    <div className="game-container">
       <h2>Game</h2>
       <p>Match found. Game screen will be here.</p>
     </div>
