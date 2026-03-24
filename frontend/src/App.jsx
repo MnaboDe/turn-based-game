0666a936d75ea934ae205f1900da89178028f95c
MnaboDe <marinkbo@gmail.com>
2026-03-24 11:05:44 +0100

    Show only login screen in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import Login from "./screens/Login";
-import Lobby from "./screens/Lobby";
-import Game from "./screens/Game";
 
 function App() {
   return (
     <main>
       <h1>Turn-Based Game</h1>
       <Login />
-      <Lobby />
-      <Game />
-      <button>Find Match</button>
     </main>
   );
 }
