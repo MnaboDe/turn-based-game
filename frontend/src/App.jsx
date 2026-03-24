59f006d88a68bf8ef023d937914a1330b773de57
MnaboDe <marinkbo@gmail.com>
2026-03-24 11:04:55 +0100

    Add login screen component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Login from "./screens/Login";
 import Lobby from "./screens/Lobby";
 import Game from "./screens/Game";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <main>
       <h1>Turn-Based Game</h1>
+      <Login />
       <Lobby />
       <Game />
       <button>Find Match</button>
---
+++ frontend/src/screens/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <p>Login screen will be here.</p>
+    </div>
+  );
+}
+
+export default Login;
