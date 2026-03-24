cc85b976f99b131661a38ac1aa2940f836c3d364
MnaboDe <marinkbo@gmail.com>
2026-03-24 11:02:55 +0100

    Add game screen component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Lobby from "./screens/Lobby";
+import Game from "./screens/Game";
 
 function App() {
   return (
     <main>
       <h1>Turn-Based Game</h1>
       <Lobby />
+      <Game />
       <button>Find Match</button>
     </main>
   );
---
+++ frontend/src/screens/Game.jsx
@@ -0,0 +1,10 @@
+function Game() {
+  return (
+    <div>
+      <h2>Game</h2>
+      <p>Game screen will be here.</p>
+    </div>
+  );
+}
+
+export default Game;
