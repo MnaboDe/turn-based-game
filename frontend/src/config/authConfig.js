3ad4d30aa8be50318dd2bd2ce6c4b8497fabe297
MnaboDe <marinkbo@gmail.com>
2026-03-28 15:54:32 +0100

    Host frontend on S3/CloudFront and update Cognito/CORS for public testing

--- frontend/src/config/authConfig.js
+++ frontend/src/config/authConfig.js
@@ -2,6 +2,6 @@ export const authConfig = {
   region: "us-east-1",
   cognitoDomain: "https://us-east-1kitzazvvy.auth.us-east-1.amazoncognito.com",
   clientId: "46rh9ho6cs79i1ojmmjhbogm8l",
-  redirectUri: "http://localhost:5173/",
+  redirectUri: "https://decou9upw106j.cloudfront.net",
   apiBaseUrl: "https://9wx2pipthb.execute-api.us-east-1.amazonaws.com",
 };
